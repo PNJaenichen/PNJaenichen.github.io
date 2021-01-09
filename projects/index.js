c33859feb9e3ff551ab483f7d6a82ea5e0164647
PNJaenichen <PNJaenichen@gmail.com>
2021-01-09 00:08:10 -0500

    Add past projects and editing the project page
    Because
    - past projects were unhosted so this moved them to a folder for hosting
    - the project page was blank
    
    I renamed the media folder to assets to beter capture the purpose of the folder.
    I also found an article from James Futhey on medium.com called "Simulating Website
    Thumbnails using iFrames" from 2016 and that's what I used to create the projects
    page. Next up should be to make them links so that they can visit but each page
    needs a return button/link.

---
+++ projects/index.js
@@ -0,0 +1,17 @@
+function formatDate(input) {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var buildYear = input.getFullYear();
+    var buildMonth = months[input.getMonth()];
+    var buildDate = input.getDate();
+    return buildDate + " " + buildMonth + " " + buildYear
+}
+function dayDifference(input1, input2) {
+    var diffTime = input1.getTime() - input2.getTime();
+    return Math.floor(diffTime / (1000 * 3600 * 24));
+}
+var dates = [new Date(), new Date(2020,4,14), new Date(2020,4,31)];
+document.getElementById('today').innerHTML = formatDate(dates[0]);
+document.getElementById('created').innerHTML = formatDate(dates[1]);
+document.getElementById('dayDiff1').innerHTML = dayDifference(dates[0],dates[1]);
+document.getElementById('updated').innerHTML = formatDate(dates[2]);
+document.getElementById('dayDiff2').innerHTML = dayDifference(dates[0],dates[2]);
