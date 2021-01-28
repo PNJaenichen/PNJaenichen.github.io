ac63b119a85c8481f8fa8b2d670668856762ba3c
PNJaenichen <PNJaenichen@gmail.com>
2021-01-27 19:45:43 -0500

    Added HTML/CSS for gameboard

---
+++ projects/TicTacToe/tictactoe.js
@@ -0,0 +1,28 @@
+// Limit global code
+// ROT: if you only need ONE of something (gameBoard, displayController),
+// use a module. If you need multiples of something (players!), create
+// them with factories
+
+// TODO save gameboard as array in Gameboard object
+
+// TODO create player objects
+
+// TODO create game flow object
+
+// TODO Create HTML and JS to render the gameboard array
+
+// TODO create function to allow player to mark a spot by clicking on the
+// board location, make sure it doesn't change if it's already taken
+
+// Each function should be worked into game, player, or gameboard objects
+
+// TODO Create logic to check for game over, win condition and ties
+
+// TODO Create interface to take and display player name
+
+// TODO Include start/restart button
+
+// TODO create display element that congratulates the winning player
+
+// OPTIONAL TODO Create AI for Computer
+// Start with picking legal move and then make it smart
