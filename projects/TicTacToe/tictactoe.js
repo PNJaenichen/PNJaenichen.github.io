a057668a4acccee8e991660ed904fa1ed263800c
PNJaenichen <PNJaenichen@gmail.com>
2021-01-29 22:24:58 -0500

    Started creating functions for player and gameboard

--- projects/TicTacToe/tictactoe.js
+++ projects/TicTacToe/tictactoe.js
@@ -4,8 +4,19 @@
 // them with factories
 
 // TODO save gameboard as array in Gameboard object
+const Gameboard = () => {
+    let topRow = ["","",""];
+    let middleRow = ["","",""];
+    let bottomRow = ["","",""];
+    return {topRow, middleRow, bottomRow}
+};
 
 // TODO create player objects
+const Player = (name, side) => {
+    const getName = () => name;
+    const getSide = () => side;
+    return {getName, getSide}
+};
 
 // TODO create game flow object
 
@@ -17,7 +28,29 @@
 // Each function should be worked into game, player, or gameboard objects
 
 // TODO Create logic to check for game over, win condition and ties
-
+function checkForWin(board) {
+    const boardState = [board.topRow, board.middleRow, board.bottomRow];
+    const diagOne = [boardState[0][0], boardState[1][1], boardState[2,2]]
+    const diagTwo = [boardState[0][2], boardState[1][1], boardState[2,0]]
+    for (let i = 0; i < 3; i++) {
+        if (boardState[i].every(x => x === boardState[i][0])) {
+            return true;
+        } else if (diagOne.every(x => x === diagOne[0])) {
+            return true;
+        } else if (diagTwo.every(x => x === diagTwo[0])) {
+            return true;
+        } else {
+            let column = [];
+            for (let j = 0; j < 3; j++) {
+                column.push(boardState[i][j])
+            }
+            if (column.every(x => x === x[0])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 // TODO Create interface to take and display player name
 
 // TODO Include start/restart button
@@ -26,3 +59,5 @@
 
 // OPTIONAL TODO Create AI for Computer
 // Start with picking legal move and then make it smart
+
+var gameBoard = Gameboard();
