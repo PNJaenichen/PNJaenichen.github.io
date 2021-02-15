f3555f17a1b29af685b7504b56241a2ab99dd931
PNJaenichen <PNJaenichen@gmail.com>
2021-02-15 10:31:39 -0500

    Added donation page and Resize function for logo div

--- projects/bpPage/main.js
+++ projects/bpPage/main.js
@@ -10,4 +10,19 @@ const bpBirthday = new Date("2021-03-13");
 
 var difference = dateDiffInDays(currentDate, bpBirthday);
 var counter = document.getElementById('countdown');
-counter.textContent = `${difference} Days To 47!`;
+if (counter) {
+    counter.textContent = `${difference} Days To 47!`;
+}
+
+var logo = document.getElementById('logo');
+var logoRatio = Math.floor(window.innerWidth * .3);
+window.addEventListener('resize', function() {
+    clearTimeout(window.resizedFinished);
+    window.resizedFinished = setTimeout(function() {
+        logoRatio = Math.floor(window.innerWidth * .3);
+        console.log(`Resized finished: ${logoRatio}`);
+        logo.style.height = `${logoRatio}px`
+        logo.style.width = `${logoRatio}px`
+    }, 250);
+});
+;
