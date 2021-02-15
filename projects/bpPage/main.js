74a89bcdb8ce3249fb233597dc1b926c06f03311
PNJaenichen <PNJaenichen@gmail.com>
2021-02-14 22:48:23 -0500

    Started bP's benefit page

---
+++ projects/bpPage/main.js
@@ -0,0 +1,13 @@
+function dateDiffInDays(a, b) {
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+}
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+const currentDate = new Date();
+const bpBirthday = new Date("2021-03-13");
+
+var difference = dateDiffInDays(currentDate, bpBirthday);
+var counter = document.getElementById('countdown');
+counter.textContent = `${difference} Days To 47!`;
