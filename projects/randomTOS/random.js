ec7d115602c5d5c04dfdd70e6f5ed14542d90adc
PNJaenichen <PNJaenichen@gmail.com>
2021-03-09 21:53:28 -0500

    Started image slider on random projects page

---
+++ projects/randomTOS/random.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+carousel();
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName('mySlides');
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+function carousel() {
+    plusDivs(1);
+    setTimeout(carousel, 5000);
+}
