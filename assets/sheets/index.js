7fe64088c22eaee80a756d66b5b49be1957eb923
PNJaenichen <PNJaenichen@gmail.com>
2021-03-13 21:56:04 -0500

    Updated date counter for index page to decrease clutter on main page.

--- assets/sheets/index.js
+++ assets/sheets/index.js
@@ -9,9 +9,10 @@ function dayDifference(input1, input2) {
     var diffTime = input1.getTime() - input2.getTime();
     return Math.floor(diffTime / (1000 * 3600 * 24));
 }
-var dates = [new Date(), new Date(2021,0,4), new Date(2021,2,10)];
-document.getElementById('today').innerHTML = formatDate(dates[0]);
-document.getElementById('created').innerHTML = formatDate(dates[1]);
-document.getElementById('dayDiff1').innerHTML = dayDifference(dates[0],dates[1]);
-document.getElementById('updated').innerHTML = formatDate(dates[2]);
-document.getElementById('dayDiff2').innerHTML = dayDifference(dates[0],dates[2]);
+var dates = [new Date(), new Date(2021,0,4), new Date(2021,2,13)];
+
+var statement = document.createElement('p');
+
+statement.innerText = `Today is ${formatDate(dates[0])}. This page was created on ${formatDate(dates[1])} and that was ${dayDifference(dates[0],dates[1])} days ago. It was last updated on ${formatDate(dates[2])} which ${dayDifference(dates[0],dates[2]) === 0 ? 'is today.' : dayDifference(dates[0],dates[2]) === 1 ? 'was yesterday!' : 'was ' + dayDifference(dates[0],dates[2]) + ' days ago.'}`;
+
+document.getElementById('dayCounts').appendChild(statement);
