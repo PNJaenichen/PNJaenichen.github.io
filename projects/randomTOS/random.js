462b23c34785f89e150c872a86c082d1c8081052
PNJaenichen <PNJaenichen@gmail.com>
2021-03-14 20:56:00 -0400

    Added in navigation dots for image slider

--- projects/randomTOS/random.js
+++ projects/randomTOS/random.js
@@ -2,19 +2,32 @@ var slideIndex = 1;
 showDivs(slideIndex);
 carousel();
 
+
 function plusDivs(n) {
     showDivs(slideIndex += n);
+}
+
+function showImage(n) {
+    showDivs(n);
 }
 
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName('mySlides');
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
+    var y = document.getElementsByClassName('slideDot');
+    if (n > x.length) {
+        slideIndex = 1
+    } else if (n < 1) {
+        slideIndex = x.length
+    } else {
+        slideIndex = n;
+    }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
+        y[i].style.backgroundColor = 'white';
     }
     x[slideIndex-1].style.display = "block";
+    y[slideIndex-1].style.backgroundColor = 'darkslategray';
 }
 
 function carousel() {
