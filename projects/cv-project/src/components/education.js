07d43bafd2ddf8d9803822dc90820ee99a58f5bf
PNJaen <PNJaenichen@gmail.com>
2021-04-17 20:40:06 -0700

    Started logic for eduation section of cv-project

--- projects/cv-project/src/components/education.js
+++ projects/cv-project/src/components/education.js
@@ -8,13 +8,45 @@ import React, { Component } from 'react'
 class Education extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      education: [] 
+    }
+    this.getInformation = this.getInformation.bind(this)
+    this.addEducation = this.addEducation.bind(this)
+  }
+
+  addEducation() {
+    this.setState(prevState => {
+      const newEd = {
+        school: document.getElementById('schoolName').value,
+        study: document.getElementById('study').value,
+        start: document.getElementById('startDate').value,
+        end: document.getElementById('endDate').value
+      }
+      return {education: [...prevState.education, newEd]}
+    })
+  }
+
+  getInformation() {
+    return (
+      <div>
+        <label for='schoolName'>School:</label>
+        <input type='text' id='schoolName' name='schoolName'></input>
+        <label for='study'>Study:</label>
+        <input type='text' id='study' name='study'></input>
+        <label for='startDate'>Start:</label>
+        <input type='text' id='startDate' name='startDate'></input>
+        <label for='endDate'>End:</label>
+        <input type='text' id='endDate' name='endDate'></input>
+        <input type='submit' onClick={this.addEducation}>Submit</input>
+      </div>
+    )
   }
 
   render() {
     return (
       <div>
-        <p>This is an education placeholder.</p>  
+        {this.getInformation()}  
       </div>
     )
   }
