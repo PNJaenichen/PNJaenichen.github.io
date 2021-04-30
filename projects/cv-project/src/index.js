33e5fc79a7fe449997a757d5ae2b70e79ecd8235
PNJaenichen <PNJaenichen@gmail.com>
2021-04-30 14:25:06 -0400

    Attempting to fix React App on GitHub Pages

--- projects/cv-project/src/index.js
+++ projects/cv-project/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import App from './App';
 import './cv-project.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
