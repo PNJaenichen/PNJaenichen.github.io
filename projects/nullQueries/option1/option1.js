79130e0490a6a68675c1ac8b3487d5bd3d4b0639
PNJaenichen <PNJaenichen@gmail.com>
2021-06-01 20:41:47 -0400

    added drop in menu on scroll function to option 1 and option 2

--- projects/nullQueries/option1/option1.js
+++ projects/nullQueries/option1/option1.js
@@ -2,6 +2,14 @@ import { API_KEY } from './test.js';
 
 const channel_id = 'UCtKKhu8MzKSde-X-xVH-Vpw'
 
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    document.getElementById('navbar').style.top = '0';
+  } else {
+    document.getElementById('navbar').style.top = '-50px'; 
+  }
+}
+
 async function setRecent() {
   const response = await fetch(`https://youtube.googleapis.com/youtube/v3/activities?part=contentDetails&channelId=${channel_id}&key=${API_KEY}`);
   const videoList = await response.json();
@@ -9,5 +17,7 @@ async function setRecent() {
   const vidFrame = document.getElementById('recentVideo')
   vidFrame.src = `https://www.youtube.com/embed/${recentVideo}?html5=1`
 }
+
+window.onscroll = function() {scrollFunction()};
 
 API_KEY ? setRecent() : console.log('No Key');
