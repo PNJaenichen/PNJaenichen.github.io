7afa72665a28df77999bb83115ab52ce85b06f77
PNJaenichen <PNJaenichen@gmail.com>
2021-06-01 20:48:02 -0400

    Fixing menu drop functionality

--- projects/nullQueries/option1/option1.js
+++ projects/nullQueries/option1/option1.js
@@ -2,14 +2,6 @@ import { API_KEY } from './test.js';
 
 const channel_id = 'UCtKKhu8MzKSde-X-xVH-Vpw'
 
-function scrollFunction() {
-  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-    document.getElementById('navbar').style.top = '0';
-  } else {
-    document.getElementById('navbar').style.top = '-50px'; 
-  }
-}
-
 async function setRecent() {
   const response = await fetch(`https://youtube.googleapis.com/youtube/v3/activities?part=contentDetails&channelId=${channel_id}&key=${API_KEY}`);
   const videoList = await response.json();
@@ -18,6 +10,5 @@ async function setRecent() {
   vidFrame.src = `https://www.youtube.com/embed/${recentVideo}?html5=1`
 }
 
-window.onscroll = function() {scrollFunction()};
 
 API_KEY ? setRecent() : console.log('No Key');
