b5a15816e6f48ad89a6c62cb42a77f6352704dad
PNJaenichen <PNJaenichen@gmail.com>
2021-07-03 21:38:18 -0400

    Added logos to TeamInfo and rendered logo for game display

--- projects/hockeydataviz/src/components/BuildDailyGames/BuildDailyGames.js
+++ projects/hockeydataviz/src/components/BuildDailyGames/BuildDailyGames.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './BuildDailyGames.css';
+import { TeamInfo } from '../../util/TeamInfo';
 
 export class BuildDailyGames extends React.Component {
   constructor(props) {
@@ -9,12 +10,20 @@ export class BuildDailyGames extends React.Component {
 
   createGame(gameInfo) {
     return gameInfo.map(game => {
+      const homeTeam = TeamInfo[game.teams.home.team.name] ? TeamInfo[game.teams.home.team.name] : {};
+      const awayTeam = TeamInfo[game.teams.away.team.name] ? TeamInfo[game.teams.away.team.name] : {};
       return (
         <div key={game.gamePk} id={game.gamePk} className='gameDisplay'>
           <div className='gameTeams'>
-            <p>{`${game.teams.away.team.name}`}</p>
+            <div className='awayTeam'>
+              <img src={awayTeam.logo} alt={`${awayTeam.abbreviation ? awayTeam.abbreviation : 'No'} logo`}></img>
+              <p>{`${game.teams.away.team.name}`}</p>
+            </div>
             <p>at</p>
-            <p>{`${game.teams.home.team.name}`}</p>
+            <div className='homeTeam'>
+              <img src={homeTeam.logo} alt={`${homeTeam.abbreviation ? homeTeam.abbreviation : 'No'} logo`}></img>
+              <p>{`${game.teams.home.team.name}`}</p>
+            </div>
           </div>
           <div className='gameScores'>
             <p>{`${game.teams.away.score}`}</p> 
--- projects/hockeydataviz/src/util/TeamInfo.js
+++ projects/hockeydataviz/src/util/TeamInfo.js
@@ -4,217 +4,217 @@ export const TeamInfo = {
     city: 'Newark',
     abbreviation: 'NJD',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/njd.png'
   },
   'New York Islanders': {
     id: 2, 
     city: 'Uniondale',
     abbreviation: 'NYI',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/nyi.png'
   },
   'New York Rangers': {
     id: 3, 
     city: 'New York',
     abbreviation: 'NYR',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/nyr.png'
   },
   'Philadelphia Flyers': {
     id: 4, 
     city: 'Philadelphia',
     abbreviation: 'PHI',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/phi.png'
   },
   'Pittsburgh Penguins': {
     id: 5, 
     city: 'Pittsburgh',
     abbreviation: 'PIT',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/pit.png'
   },
   'Boston Bruins': {
     id: 6, 
     city: 'Boston',
     abbreviation: 'BOS',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/bos.png'
   },
   'Buffalo Sabres': {
     id: 7, 
     city: 'Buffalo',
     abbreviation: 'BUF',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/buf.png'
   },
   'Montréal Canadiens': {
     id: 8, 
     city: 'Montréal',
     abbreviation: 'MTL',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/mtl.png'
   },
   'Ottawa Senators': {
     id: 9, 
     city: 'Ottawa',
     abbreviation: 'OTT',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/ott.png'
   },
   'Toronto Maple Leafs': {
     id: 10, 
     city: 'Toronto',
     abbreviation: 'TOR',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/tor.png'
   },
   'Carolina Hurricanes': {
     id: 12, 
     city: 'Raleigh',
     abbreviation: 'CAR',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/car.png'
   },
   'Florida Panthers': {
     id: 13, 
     city: 'Sunrise',
     abbreviation: 'FLA',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/fla.png'
   },
   'Tampa Bay Lightning': {
     id: 14, 
     city: 'Tampa',
     abbreviation: 'TBL',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/tb.png'
   },
   'Washington Capitals': {
     id: 15, 
     city: 'Washington',
     abbreviation: 'WSH',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/wsh.png'
   },
   'Chicago Blackhawks': {
     id: 16, 
     city: 'Chicago',
     abbreviation: 'CHI',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/chi.png'
   },
   'Detroit Red Wings': {
     id: 17, 
     city: 'Detroit',
     abbreviation: 'DET',
     conference: 'Eastern',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/det.png'
   },
   'Nashville Predators': {
     id: 18, 
     city: 'Nashville',
     abbreviation: 'NSH',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/nsh.png'
   },
   'St. Louis Blues': {
     id: 19, 
     city: 'St. Louis',
     abbreviation: 'STL',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/stl.png'
   },
   'Calgary Flames': {
     id: 20, 
     city: 'Calgary',
     abbreviation: 'CGY',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/cgy.png'
   },
   'Colorado Avalanche': {
     id: 21, 
     city: 'Denver',
     abbreviation: 'COL',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/col.png'
   },
   'Edmonton Oilers': {
     id: 22, 
     city: 'Edmonton',
     abbreviation: 'EDM',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/edm.png'
   },
   'Vancouver Canucks': {
     id: 23, 
     city: 'Vancouver',
     abbreviation: 'VAN',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/van.png'
   },
   'Anaheim Ducks': {
     id: 24, 
     city: 'Anaheim',
     abbreviation: 'ANA',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/ana.png'
   },
   'Dallas Stars': {
     id: 25, 
     city: 'Dallas',
     abbreviation: 'DAL',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/dal.png'
   },
   'Los Angeles Kings': {
     id: 26, 
     city: 'Los Angeles',
     abbreviation: 'LAK',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/la.png'
   },
   'San Jose Sharks': {
     id: 28, 
     city: 'San Jose',
     abbreviation: 'SJS',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/sj.png'
   },
   'Columbus Blue Jackets': {
     id: 29, 
     city: 'Columbus',
     abbreviation: 'CBJ',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/cbj.png'
   },
   'Minnesota Wild': {
     id: 30, 
     city: 'St. Paul',
     abbreviation: 'MIN',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/min.png'
   },
   'Winnipeg Jets': {
     id: 52, 
     city: 'Winnipeg',
     abbreviation: 'WPG',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/wpg.png'
   },
   'Arizona Coyotes': {
     id: 53, 
     city: 'Glendale',
     abbreviation: 'ARI',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/ari.png'
   },
   'Vegas Golden Knights': {
     id: 54, 
     city: 'Las Vegas',
     abbreviation: 'VGK',
     conference: 'Western',
-    logo: ''
+    logo: 'https://a.espncdn.com/combiner/i?img=/i/teamlogos/nhl/500/vgs.png'
   },
   'Seattle Kraken': {
     id: 55, 
