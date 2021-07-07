b30931cad889409a5f3cd07f062c0a0befd69538
PNJaenichen <PNJaenichen@gmail.com>
2021-07-06 20:16:32 -0400

    started game page, created layout

--- projects/hockeydataviz/src/components/GameDisplay/GameDisplay.js
+++ projects/hockeydataviz/src/components/GameDisplay/GameDisplay.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { TeamInfo } from '../../util/TeamInfo';
+import './GameDisplay.css';
 
 export class GameDisplay extends React.Component {
   constructor(props) {
@@ -14,7 +16,17 @@ export class GameDisplay extends React.Component {
     return (
       <div>
         <button onClick={this.handleReturn}>Return</button>
-        <p>{JSON.stringify(this.props.gameData)}</p>
+        <div className='scoreBoard'>
+          <div className='awayTeamGame'>
+            <img src={TeamInfo[this.props.gameData.gameData.teams.away.name].logo} alt='Away Team Logo'></img>
+            <p>{this.props.gameData.liveData.linescore.teams.away.goals}</p>
+          </div>
+          <p>:</p>
+          <div className='homeTeamGame'>
+            <p>{this.props.gameData.liveData.linescore.teams.home.goals}</p>
+            <img src={TeamInfo[this.props.gameData.gameData.teams.home.name].logo} alt='Home Team Logo'></img>
+          </div>
+        </div>
       </div>
     )
   }
