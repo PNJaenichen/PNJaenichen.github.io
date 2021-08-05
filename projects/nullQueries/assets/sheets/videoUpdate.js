5ae72641f6d8ab5d58a128af621aca749026d540
PNJaenichen <PNJaenichen@gmail.com>
2021-08-04 20:35:39 -0400

    nullQueries: trying to make things work

--- projects/nullQueries/assets/sheets/videoUpdate.js
+++ projects/nullQueries/assets/sheets/videoUpdate.js
@@ -1,14 +1,16 @@
-import { API_KEY } from './key.js';
+// import { API_KEY } from './key.js';
 
-const channel_id = 'UCtKKhu8MzKSde-X-xVH-Vpw'
+// const channel_id = 'UCtKKhu8MzKSde-X-xVH-Vpw'
 
-async function setRecent() {
-  const response = await fetch(`https://youtube.googleapis.com/youtube/v3/activities?part=contentDetails&channelId=${channel_id}&key=${API_KEY}`);
-  const videoList = await response.json();
-  const recentVideo = videoList.items[0].contentDetails.upload.videoId;
-  const vidFrame = document.getElementById('recentVideo')
-  vidFrame.src = `https://www.youtube.com/embed/${recentVideo}?html5=1`
-}
+// async function setRecent() {
+//   const response = await fetch(`https://youtube.googleapis.com/youtube/v3/activities?part=contentDetails&channelId=${channel_id}&key=${API_KEY}`);
+//   const videoList = await response.json();
+//   const recentVideo = videoList.items[0].contentDetails.upload.videoId;
+//   const vidFrame = document.getElementById('recentVideo')
+//   vidFrame.src = `https://www.youtube.com/embed/${recentVideo}?html5=1`
+// }
+
+// API_KEY ? setRecent() : '';
 
 function init() {
   const vidDefer = document.getElementsByTagName('iframe');
@@ -20,5 +22,4 @@ function init() {
   }
 }
 
-API_KEY ? setRecent() : '';
 window.onload = init();
