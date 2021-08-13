b675728a88827ddc41f9f6183b519313eb7219eb
PNJaenichen <PNJaenichen@gmail.com>
2021-08-12 20:52:58 -0400

    refactoring blog page

--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -9,14 +9,21 @@ import { posts } from "./posts";
 
 export default class Blog extends React.Component {
   cardCreater() {
+    let uniqID = 0;
     return posts.map(post => {
+      uniqID += 1;
+      let paraID = 0;
       const subDate = post.subtitle ? `${post.subtitle}; ${post.date}` : post.date;
       return (
-        <div className="card">
+        <div key={`blogPost${uniqID}`} className="card">
           <h3>{post.title}</h3>
           <h4>{subDate}</h4>
           <div className="fakeimg"><img src={post.image} alt="Empty" height="100%" /></div>
-          {post.text.map(paragraph => <p>{paragraph}</p>)}
+          {post.text.map(paragraph => {
+            paraID += 1;
+            return <p key={`para${uniqID}${paraID}`}>{paragraph}</p>
+            }
+          )}
         </div> 
       )
     })
@@ -34,12 +41,14 @@ export default class Blog extends React.Component {
           <div className="rightcolumn">
             <div className="card">
               <h3>About Me</h3>
-              <div class="fakeimg" style={{height: "100px"}}><img src={pixelKiwi} style={{height: "100px"}} alt="pixelated author" /></div>
+              <div className="fakeimg" style={{height: "100px"}}><img src={pixelKiwi} style={{height: "100px"}} alt="pixelated author" /></div>
             </div>
           </div>
-          <Link to="/">Return to Main Page</Link>
         </main>
         <Footer />
+        <footer>
+          <Link to="/">Return to Main Page</Link>
+        </footer>
       </div>
     );
   }
