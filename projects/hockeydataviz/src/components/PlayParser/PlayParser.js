d179f1ed5e792677ebd2dc6717d4c2436e5f1fa7
PNJaenichen <PNJaenichen@gmail.com>
2021-09-05 23:11:41 -0400

    continued working forward on parsing player on ice data

--- projects/hockeydataviz/src/components/PlayParser/PlayParser.js
+++ projects/hockeydataviz/src/components/PlayParser/PlayParser.js
@@ -4,8 +4,7 @@ export default function PlayParser(htmlReport) {
   const bodyReport = el.getElementsByTagName('body')[0];
   const childEl = [...bodyReport.children];
   const pagesChi = childEl.slice(2, childEl.length - 1);
-  pagesChi.forEach(element => {
-    console.log(element.children[0].children[0].children)
-  });
+  const pageBreakDown = pagesChi.map(element => [...element.children[0].children[0].children]);
+  console.log(pageBreakDown[0][6]);
   return bodyReport.innerHTML;
 } 
