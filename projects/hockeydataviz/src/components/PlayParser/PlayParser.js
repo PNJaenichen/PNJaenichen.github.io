b39fa8ae4f7be6b82e9153de63d7ed9f7345036d
PNJaenichen <PNJaenichen@gmail.com>
2021-09-06 20:28:55 -0400

    parsed players in a play, need to iterate through all plays

--- projects/hockeydataviz/src/components/PlayParser/PlayParser.js
+++ projects/hockeydataviz/src/components/PlayParser/PlayParser.js
@@ -5,6 +5,8 @@ export default function PlayParser(htmlReport) {
   const childEl = [...bodyReport.children];
   const pagesChi = childEl.slice(2, childEl.length - 1);
   const pageBreakDown = pagesChi.map(element => [...element.children[0].children[0].children]);
-  console.log(pageBreakDown[0][6]);
+  const testFilter = [...pageBreakDown[7][11].children].slice(6,);
+  const testReplace = testFilter.map(element => element.innerText.replace(/\s+/g, "").split(/[A-Z]/g));
+  console.log(testReplace.map(element => element.filter(element => element !== ""))) 
   return bodyReport.innerHTML;
 } 
