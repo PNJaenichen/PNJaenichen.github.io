200987d9fded5a2510c04afa7bc19e8130302529
PNJaenichen <PNJaenichen@gmail.com>
2021-09-07 22:41:44 -0400

    successfully mapped through and combined all plays, still need to split times and return

--- projects/hockeydataviz/src/components/PlayParser/PlayParser.js
+++ projects/hockeydataviz/src/components/PlayParser/PlayParser.js
@@ -4,9 +4,12 @@ export default function PlayParser(htmlReport) {
   const bodyReport = el.getElementsByTagName('body')[0];
   const childEl = [...bodyReport.children];
   const pagesChi = childEl.slice(2, childEl.length - 1);
-  const pageBreakDown = pagesChi.map(element => [...element.children[0].children[0].children]);
-  const testFilter = [...pageBreakDown[7][11].children].slice(6,);
-  const testReplace = testFilter.map(element => element.innerText.replace(/\s+/g, "").split(/[A-Z]/g));
-  console.log(testReplace.map(element => element.filter(element => element !== ""))) 
+  const pageArray = pagesChi.map(element => [...element.children[0].children[0].children]);
+  const pageBreakDown = pageArray.map(element => element.map(row => [...row.children]));
+  const rowBreakDown = pageBreakDown.map(row => row.filter(element => element.length === 8));
+  const playBreakDown = rowBreakDown.map(row => row.map(element => [element[0].innerText, element[1].innerText, element[3].innerText, element[6].innerText.replace(/\s+/g, "").split(/[A-Z]/g), element[7].innerText.replace(/\s+/g, "").split(/[A-Z]/g)]));
+  const filteredPlays = playBreakDown.map(row => row.filter(element => !isNaN(parseInt(element[4][0]))));
+  const playList = Array.prototype.concat.apply([], filteredPlays)
+  console.log(playList)
   return bodyReport.innerHTML;
 } 
