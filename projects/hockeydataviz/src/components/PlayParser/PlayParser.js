4dd5fc517e973cefacba4cd41ea8c3203d58bb52
PNJaen <PNJaenichen@gmail.com>
2021-09-10 19:24:04 -0700

    small refactor for regex for breaking down players on ice for each play

--- projects/hockeydataviz/src/components/PlayParser/PlayParser.js
+++ projects/hockeydataviz/src/components/PlayParser/PlayParser.js
@@ -1,4 +1,6 @@
 export default function PlayParser(htmlReport) {
+  const spaces = /\s+/g;
+  const digits = /\d+/g;
   const el = document.createElement('html');
   el.innerHTML = htmlReport;
   const bodyReport = el.getElementsByTagName('body')[0];
@@ -7,7 +9,7 @@ export default function PlayParser(htmlReport) {
   const pageArray = pagesChi.map(element => [...element.children[0].children[0].children]);
   const pageBreakDown = pageArray.map(element => element.map(row => [...row.children]));
   const rowBreakDown = pageBreakDown.map(row => row.filter(element => element.length === 8));
-  const playBreakDown = rowBreakDown.map(row => row.map(element => [element[0].innerText, element[1].innerText, element[3].innerText.match(/(\d{1,2}:\d{2})/g), element[6].innerText.replace(/\s+/g, "").match(/\d+/g), element[7].innerText.replace(/\s+/g, "").match(/\d+/g)]));
+  const playBreakDown = rowBreakDown.map(row => row.map(element => [element[0].innerText, element[1].innerText, element[3].innerText.match(/(\d{1,2}:\d{2})/g), element[6].innerText.replace(spaces, "").match(digits), element[7].innerText.replace(spaces, "").match(digits)]));
   const filteredPlays = playBreakDown.map(row => row.filter(element => element[3] !== null));
   const playList = Array.prototype.concat.apply([], filteredPlays)
   return playList;
