a1bd53d58a6d540c994c50afa57f6064bd380017
PNJaen <PNJaenichen@gmail.com>
2021-09-11 09:40:15 -0400

    Refactor and trying to make playID from game summary match to that in the NHL API

--- projects/hockeydataviz/src/components/PlayParser/PlayParser.js
+++ projects/hockeydataviz/src/components/PlayParser/PlayParser.js
@@ -1,16 +1,24 @@
+const spaces = /\s+/g;
+const digits = /\d+/g;
+
 export default function PlayParser(htmlReport) {
-  const spaces = /\s+/g;
-  const digits = /\d+/g;
+  // get the html from the report page
   const el = document.createElement('html');
   el.innerHTML = htmlReport;
-  const bodyReport = el.getElementsByTagName('body')[0];
-  const childEl = [...bodyReport.children];
-  const pagesChi = childEl.slice(2, childEl.length - 1);
-  const pageArray = pagesChi.map(element => [...element.children[0].children[0].children]);
+
+  // the document is seperated into 'pages', pull those pages and then put the rows into an array
+  const childEl = [...el.getElementsByTagName('body')[0].children];
+  const pageArray = childEl.slice(2, childEl.length - 1).map(element => [...element.children[0].children[0].children]);
   const pageBreakDown = pageArray.map(element => element.map(row => [...row.children]));
+
+  // remove all the plays that do not have player informatin, like game start, period start, etc
   const rowBreakDown = pageBreakDown.map(row => row.filter(element => element.length === 8));
-  const playBreakDown = rowBreakDown.map(row => row.map(element => [element[0].innerText, element[1].innerText, element[3].innerText.match(/(\d{1,2}:\d{2})/g), element[6].innerText.replace(spaces, "").match(digits), element[7].innerText.replace(spaces, "").match(digits)]));
-  const filteredPlays = playBreakDown.map(row => row.filter(element => element[3] !== null));
-  const playList = Array.prototype.concat.apply([], filteredPlays)
-  return playList;
+  
+  // pull the playId, period number, time stamp, away team, and home team information
+  const playBreakDown = rowBreakDown.map(row => row.map(element => {
+    return [element[0].innerText, element[1].innerText, element[3].innerText.match(/(\d{1,2}:\d{2})/g), element[6].innerText.replace(spaces, "").match(digits), element[7].innerText.replace(spaces, "").match(digits), element[4].innerText]
+  }));
+  
+  // add it all to a single array and return it
+  return Array.prototype.concat.apply([], playBreakDown.map(row => row.filter(element => element[3] !== null && element[4] !== null)))
 } 
