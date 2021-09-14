adc4ebcd2cb6c5735cacebd227e4cd6c0a2e93c8
PNJaenichen <PNJaenichen@gmail.com>
2021-09-13 21:54:17 -0400

    readjusted game display on calender section for list of daily games

--- projects/hockeydataviz/src/components/BuildDailyGames/BuildDailyGames.js
+++ projects/hockeydataviz/src/components/BuildDailyGames/BuildDailyGames.js
@@ -14,6 +14,7 @@ export class BuildDailyGames extends React.Component {
   }
 
   createGame(gameInfo) {
+    console.log(gameInfo)
     return gameInfo.map(game => {
       const homeTeam = TeamInfo[game.teams.home.team.name] ? TeamInfo[game.teams.home.team.name] : {};
       const awayTeam = TeamInfo[game.teams.away.team.name] ? TeamInfo[game.teams.away.team.name] : {};
@@ -23,18 +24,18 @@ export class BuildDailyGames extends React.Component {
             <div className='awayTeam'>
               <img src={awayTeam.logo} alt={`${awayTeam.abbreviation ? awayTeam.abbreviation : 'No'} logo`}></img>
               <p>{`${game.teams.away.team.name}`}</p>
+              <p>{`${game.teams.away.score}`}</p> 
             </div>
-            <p>at</p>
+            <div className="venue">
+              <p>at</p>
+              <p>{`${game.venue.name}`}</p>
+            </div>
             <div className='homeTeam'>
               <img src={homeTeam.logo} alt={`${homeTeam.abbreviation ? homeTeam.abbreviation : 'No'} logo`}></img>
               <p>{`${game.teams.home.team.name}`}</p>
+              <p>{`${game.teams.home.score}`}</p>
             </div>
           </div>
-          <div className='gameScores'>
-            <p>{`${game.teams.away.score}`}</p> 
-            <p>{`${game.teams.home.score}`}</p>
-          </div>
-          <p>{`${game.status.detailedState} - ${game.venue.name}`}</p>
         </div>
       )
     })
