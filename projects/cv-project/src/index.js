5936b472028182934dea4d9238610cf19444dd86
PNJaenichen <PNJaenichen@gmail.com>
2021-09-23 17:21:46 -0400

    continuing refactor of react app projects

--- projects/cv-project/src/index.js
+++ projects/cv-project/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
+import ResumeApp from './resumeApp';
 import './cv-project.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <resumeApp />
+      <ResumeApp />
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- projects/cv-project/src/resumeApp.js
+++ projects/cv-project/src/resumeApp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import General from './projects/cv-project/components/general'
-import Education from './projects/cv-project/components/education'
-import Experience from './projects/cv-project/components/experience'
+import General from './components/projects/cv-project/components/general'
+import Education from './components/projects/cv-project/components/education'
+import Experience from './components/projects/cv-project/components/experience'
 
 class resumeApp extends Component {
   constructor() {
