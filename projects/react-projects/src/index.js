389479990c02064560e16b29b8d6db0a8dfbaeee
PNJaenichen <PNJaenichen@gmail.com>
2021-09-24 20:45:47 -0400

    refactor of projects

--- projects/react-projects/src/index.js
+++ projects/react-projects/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { HashRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import ResumeApp from './resumeApp';
