7dc10b84aa7da05fffaf76f93d0bfad36c5b7065
PNJaenichen <PNJaenichen@gmail.com>
2021-10-15 22:23:23 -0400

    added function to get game information, started function to get and parse play by play data

--- projects/pythonHockey/NHLget.py
+++ projects/pythonHockey/NHLget.py
@@ -10,8 +10,11 @@ soup = BeautifulSoup(webpage, 'html.parser')
 
 def search_for_games(start, end=None):
   """
+  Gets the list of Game IDs for games played during the requested time period.
+
   Dates should be in YYYY-MM-DD format as string, with time period requested.
-  For same day, just enter a single date.
+  For same day, enter a single date. Returns a list of all games played during 
+  that time period.
   """
   web_request = requests.get(API_URL + f"schedule?startDate={start}&endDate={end if end else start}")
   data_json = web_request.json()
@@ -24,7 +27,47 @@ def search_for_games(start, end=None):
         gameList.append(game)
   return gameList
 
-test_run = search_for_games("2021-10-12", "2021-10-13")
+def get_game_info(gameID):
+  """
+  Gets game information from the NHL API. 
+  
+  Takes a single parameter, gameID, in string format.
 
-for game in test_run:
-  print(game["gamePk"])
+  Game ID is in the format YYYYTT#### where YYYY is start year of season. 
+  TT is the game type: 01 Preseason, 02 Regular Season, 03 Playoffs, and 
+  04 All Star. #### is the game number start at 0001. In the playoffs, the 
+  second digit indicates the round, the third digit indicates the matchup, 
+  and the final digit indicates the game number.
+
+  Returns JSON 
+  """
+  web_request = requests.get(API_URL + f"game/{str(gameID)}/feed/live")
+  data_json = web_request.json()
+  if len(data_json) == 2:
+    return None
+  else:
+    return data_json
+
+def get_player_on_ice_info(gameID):
+  """
+  Gets player on ice information for a game from the Official Game Play by 
+  Play document produced by the NHL via NHL.com.
+
+  Takes a single parameter, gameID, in string format.
+
+  Game ID is in the format YYYYTT#### where YYYY is start year of season. 
+  TT is the game type: 01 Preseason, 02 Regular Season, 03 Playoffs, and 
+  04 All Star. #### is the game number start at 0001. In the playoffs, the 
+  second digit indicates the round, the third digit indicates the matchup, 
+  and the final digit indicates the game number.
+
+  Returns a BeautifulSoup Object
+  """
+
+  season = f"{gameID[:4]}{int(gameID[:4]) + 1}"
+  web_request = requests.get(f"https://www.nhl.com/scores/htmlreports/{season}/PL{gameID[4:]}.HTM")
+
+  return BeautifulSoup(web_request.content, 'html.parser')
+
+
+soup = get_player_on_ice_info('2021020001')
