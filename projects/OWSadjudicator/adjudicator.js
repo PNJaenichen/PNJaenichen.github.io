b225bf332362ab5344fa6ab009fcca67fe2a572c
PNJaen <PNJaenichen@gmail.com>
2021-11-15 22:59:58 -0500

    started OWS adjuicator, created shell html and mapped out initial requirements for functions

---
+++ projects/OWSadjudicator/adjudicator.js
@@ -0,0 +1,38 @@
+/* 
+  Dice Roller
+
+  get random result based on die roll
+*/
+console.log(Math.random())
+/*
+  Surface Strike
+
+  Take incoming missiles
+  Go through adjustments for CAP, AEW, and IO
+  Calculate best odds for defender, roll against that
+ */
+
+/*
+  Submarine Attack
+
+  Detect attack, counter attack if required
+  torpedo attack
+*/
+
+/* 
+  Surface to Air
+
+  Missiles vs Aircraft
+*/
+
+/*
+  Air to Air 
+
+  Air vs. Air, simultaneous attacks
+*/
+
+/*
+  SOF Direct Action
+
+  Roll for detection, if not detected, roll for direct action
+*/
