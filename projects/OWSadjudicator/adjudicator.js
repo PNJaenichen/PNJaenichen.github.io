24316188e720c25e42afdbd590aae7ceea15893a
PNJaen <PNJaenichen@gmail.com>
2021-11-16 22:16:59 -0500

    started OWS adjuciation tool

--- projects/OWSadjudicator/adjudicator.js
+++ projects/OWSadjudicator/adjudicator.js
@@ -1,22 +1,54 @@
-/* 
-  Dice Roller
+function dieRoller(sides) {
+  return Math.floor(Math.random() * sides) + 1;
+}
 
-  get random result based on die roll
-*/
-console.log(Math.random())
+function surfaceStrike(incoming, defense=[], cap=false, aew=false, io=[]) {
+  const missiles = incoming;
+  if (defense.length > 0) {
+    let total_sm = 0;
+    for (const [key, value] of Object.entries(defense)) {
+      if (key == 0) {
+        continue
+      } else {
+        total_sm += value[1];
+      }
+    }
+    console.log(total_sm)
+  }
+  
+  const missile_rolls = []
+  for (const [key, value] of Object.entries(missiles)) {
+    for (let i = 0; i < value; i++) {
+      missile_rolls.push(dieRoller(key))
+    }
+  }
+  return missile_rolls.filter(x => x >= defense);
+}
+
+const doc = document.getElementById('testArea');
+
+const inbound_missiles = {12: 4};
+const defense = [4, [6 , 4], [5, 2]]
+const results = surfaceStrike(inbound_missiles, defense);
+doc.innerText = results;
 /*
   Surface Strike
-
-  Take incoming missiles
-  Go through adjustments for CAP, AEW, and IO
   Calculate best odds for defender, roll against that
+
+  Modifiers:
+    AEW Support (Blue Only) 2x Demo to all
+    CAP Support 1x Demo 1:1
+    Excess shields 1x Demo 1:1
+    SIGINT/EMSO 1x Promo to all
+    Disrupted C2/Active & Passive CM 1x demo to all
  */
 
 /*
   Submarine Attack
 
-  Detect attack, counter attack if required
-  torpedo attack
+  3. all units in ASW roll for detection
+     a. if successful roll for attack
+  4. Sub attacks, 1 torp per step
 */
 
 /* 
@@ -35,4 +67,10 @@ console.log(Math.random())
   SOF Direct Action
 
   Roll for detection, if not detected, roll for direct action
+*/
+
+/*
+  Ground Combat
+
+
 */
