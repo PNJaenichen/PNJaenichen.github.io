ed1600f0823b8e90c45e30456301b52ed48b32d1
PNJaenichen <PNJaenichen@gmail.com>
2021-12-03 22:17:22 -0500

    finished Day 3

--- projects/adventOfCode/2021/Day3.py
+++ projects/adventOfCode/2021/Day3.py
@@ -1,6 +1,5 @@
 def get_bit_list(list_o_bits):
   bit_lists = []
-  print(list_o_bits)
   n = len(list_o_bits[0])
   for i in range(0,n):
     bit_lists.append([])
@@ -27,40 +26,39 @@ def get_epsilon(list_o_bits):
   for bit in bit_lists:
     one_count = bit.count('1')
     zero_count = bit.count('0')
-    if one_count <= zero_count:
+    if one_count < zero_count:
       epsilon += '1'
     else:
       epsilon += '0'
   return epsilon
 
 def get_oxygen_rating(list_o_bits):
-  filtered_bits = list_o_bits.copy()
+  filtered_bits = list_o_bits
   n = len(filtered_bits[0])
-  total_bits = len(filtered_bits)
   for i in range(0, n):
-    if total_bits >= 1:
+    total_bits = len(filtered_bits)
+    if total_bits > 1:
       updated_filter = get_gamma(filtered_bits)
       filtered_bits = list(filter(lambda x: x[i] == updated_filter[i], filtered_bits))
-  return filtered_bits
+  return filtered_bits[0]
 
 def get_co2_scrubber_rating(list_o_bits):
-  filtered_bits = list_o_bits.copy()
+  filtered_bits = list_o_bits
   n = len(filtered_bits[0])
-  total_bits = len(filtered_bits)
   for i in range(0, n):
-    if total_bits >= 1:
+    total_bits = len(filtered_bits)
+    if total_bits > 1:
       updated_filter = get_epsilon(filtered_bits)
       filtered_bits = list(filter(lambda x: x[i] == updated_filter[i], filtered_bits))
-  return filtered_bits
+  return filtered_bits[0]
 
-with open('day3test.txt') as binary:
-# with open('day3.txt') as binary:
+# with open('day3test.txt') as binary:
+with open('day3.txt') as binary:
   bits = []
   for i in binary.readlines():
     bits.append(i.strip())
     
-# print(get_gamma(bits))
-# print(get_epsilon(bits))
-# print(int(get_gamma(bits), 2) * int(get_epsilon(bits), 2))
-# print(get_oxygen_rating(bits))
-print(get_co2_scrubber_rating(bits))
+print(get_gamma(bits))
+print(get_epsilon(bits))
+print(int(get_gamma(bits), 2) * int(get_epsilon(bits), 2))
+print(int(get_oxygen_rating(bits), 2) * int(get_co2_scrubber_rating(bits), 2))
