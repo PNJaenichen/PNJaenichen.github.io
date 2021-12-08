46b4aedad07ef40d7a60493f4747197b6fd2fe42
PNJaen <PNJaenichen@gmail.com>
2021-12-08 13:35:51 -0500

    completed day 6

---
+++ projects/adventOfCode/2021/Day6.py
@@ -0,0 +1,29 @@
+
+# test input
+# input = '3,4,3,1,2'
+# puzzle input
+input = '4,3,4,5,2,1,1,5,5,3,3,1,5,1,4,2,2,3,1,5,1,4,1,2,3,4,1,4,1,5,2,1,1,3,3,5,1,1,1,1,4,5,1,2,1,2,1,1,1,5,3,3,1,1,1,1,2,4,2,1,2,3,2,5,3,5,3,1,5,4,5,4,4,4,1,1,2,1,3,1,1,4,2,1,2,1,2,5,4,2,4,2,2,4,2,2,5,1,2,1,2,1,4,4,4,3,2,1,2,4,3,5,1,1,3,4,2,3,3,5,3,1,4,1,1,1,1,2,3,2,1,1,5,5,1,5,2,1,4,4,4,3,2,2,1,2,1,5,1,4,4,1,1,4,1,4,2,4,3,1,4,1,4,2,1,5,1,1,1,3,2,4,1,1,4,1,4,3,1,5,3,3,3,4,1,1,3,1,3,4,1,4,5,1,4,1,2,2,1,3,3,5,3,2,5,1,1,5,1,5,1,4,4,3,1,5,5,2,2,4,1,1,2,1,2,1,4,3,5,5,2,3,4,1,4,2,4,4,1,4,1,1,4,2,4,1,2,1,1,1,1,1,1,3,1,3,3,1,1,1,1,3,2,3,5,4,2,4,3,1,5,3,1,1,1,2,1,4,4,5,1,5,1,1,1,2,2,4,1,4,5,2,4,5,2,2,2,5,4,4'
+
+# each fish timer goes down 1, 0 becomes 6 and adds an 8 to the end of the list
+# for test input, after 18 days there are 26 fish and after 80 there are 5934
+
+list_of_fish = list(map(lambda x: int(x), input.split(',')))
+
+days = 256
+
+fish_lives = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+day_list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in list_of_fish:
+  fish_lives[i] += 1
+
+for day in range(0, days):
+  new_fish = fish_lives[0]
+  fish_lives[0] = 0
+  for i in day_list:
+    fish_lives[i - 1] = fish_lives[i]
+    fish_lives[i] = 0
+  fish_lives[6] += new_fish
+  fish_lives[8] += new_fish
+
+print(sum(fish_lives.values()))
