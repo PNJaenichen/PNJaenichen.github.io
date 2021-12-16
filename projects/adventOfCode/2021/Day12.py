af1a8362e937c2fa4012e7d162efc3267f9906ad
PNJaenichen <PNJaenichen@gmail.com>
2021-12-16 17:15:11 -0500

    Started Day 12 for Advent of Code

---
+++ projects/adventOfCode/2021/Day12.py
@@ -0,0 +1,11 @@
+# This one has 10 routes
+lines = ['start-A', 'start-b', 'A-c', 'A-b', 'b-d', 'A-end', 'b-end']
+
+# This one has 19 routes
+# lines = ['dc-end', 'HN-start', 'start-kj', 'dc-start', 'dc-HN', 'LN-dc', 'HN-end', 'kj-sa', 'kj-HN', 'kj-dc']
+
+# This one has 226 routes
+# lines = ['fs-end', 'he-DX', 'fs-he', 'start-DX', 'pj-DX', 'end-zg', 'zg-sl', 'zg-pj', 'pj-he', 'RW-he', 'fs-DX', 'pj-RW', 'zg-RW', 'start-pj', 'he-WI', 'zg-he', 'pj-fs', 'start-RW']
+
+# This is the puzzle input
+# lines = ['vn-DD', 'qm-DD', 'MV-xy', 'end-xy', 'KG-end', 'end-kw', 'qm-xy', 'start-vn', 'MV-vn', 'vn-ko', 'lj-KG', 'DD-xy', 'lj-kh', 'lj-MV', 'ko-MV', 'kw-qm', 'qm-MV', 'lj-kw', 'VH-lj', 'ko-qm', 'ko-start', 'MV-start', 'DD-ko']
