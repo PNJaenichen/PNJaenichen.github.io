21b107d01c0680ed1c6165427b1b673a3589e0c6
PNJaenichen <PNJaenichen@gmail.com>
2021-12-24 20:02:10 -0500

    finished Day 13 for Advent of Code

--- projects/adventOfCode/2021/Day13.py
+++ projects/adventOfCode/2021/Day13.py
@@ -1,131 +1,103 @@
 import numpy as np
 
-def findMaxes(coords):
-  max_x = 0
-  max_y = 0
+def getMaxes(coords):
+  max_X, max_Y = [0,0]
   for coord in coords:
-    if coord[0] > max_x:
-      max_x = coord[0]
-    if coord[1] > max_y:
-      max_y = coord[1]
-  return max_x, max_y
-
-def foldHorz(fold, coords):
-  new_coords = []
-  for coord in coords:
-    if coord[1] >= fold:
-      tot_diff = coord[1] - fold
-      new_coords.append((coord[0], fold - tot_diff))
-    else:
-      new_coords.append(coord)
-  # folded_coords = []
-  # for coord in new_coords:
-  #   if coord[0] >= fold:
-  #     folded_coords.append((coord[0] - fold - 1, coord[1]))
-  #   else:
-  #     folded_coords.append(coord)
-  return new_coords
+    max_X = coord[0] if coord[0] > max_X else max_X
+    max_Y = coord[1] if coord[1] > max_Y else max_Y
+  return max_X, max_Y
 
 def foldVert(fold, coords):
   new_coords = []
   for coord in coords:
-    if coord[0] <= fold:
-      tot_diff = fold - coord[0]
-      new_coords.append((fold + tot_diff, coord[1]))
+    if coord[1] > fold:
+      fold_dist = coord[1] - fold
+      new_coords.append((coord[0], fold - fold_dist))
     else:
       new_coords.append(coord)
-  folded_coords = []
-  for coord in new_coords:
-    if coord[0] >= fold:
-      folded_coords.append((coord[0] - fold - 1, coord[1]))
+  return new_coords
+
+def foldHorz(fold, coords):
+  new_coords = []
+  for coord in coords:
+    if coord[0] > fold:
+      fold_dist = coord[0] - fold
+      new_coords.append((fold - fold_dist, coord[1]))
     else:
-      folded_coords.append(coord)
-  return folded_coords
+      new_coords.append(coord)
+  return new_coords
 
+# lines = '''6,10
+# 0,14
+# 9,10
+# 0,3
+# 10,4
+# 4,11
+# 6,0
+# 6,12
+# 4,1
+# 0,13
+# 10,12
+# 3,4
+# 3,0
+# 8,4
+# 1,10
+# 2,14
+# 8,10
+# 9,0
 
-  
+# fold along y=7
+# fold along x=5
+# '''
 
-lines = '''6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
+with open('day13.txt') as transparency:
+  marks = []
+  folds = []
+  for i in transparency.readlines():
+    if not i.startswith('fold') and i != '\n':
+      x,y = i.strip().split(',')
+      marks.append((int(x),int(y)))
+    elif i.startswith('fold'):
+      axis, val = i.split()[2].split('=')
+      folds.append((axis, int(val)))
 
-fold along y=7
-fold along x=5
-'''
+# marks = []
+# folds = []
+# for line in lines.split('\n'):
+#   if not line.startswith('fold') and line != '':
+#     x,y = line.strip().split(',')
+#     marks.append((int(x),int(y)))
+#   elif line.startswith('fold'):
+#     axis, val = line.split()[2].split('=')
+#     folds.append((axis, int(val)))
 
-# with open('day13.txt') as transparency:
-#   marks = []
-#   folds = []
-#   for i in transparency.readlines():
-#     if not i.startswith('fold') and i != '\n':
-#       x,y = i.strip().split(',')
-#       marks.append((int(x),int(y)))
-#     elif i.startswith('fold'):
-#       axis, val = i.split()[2].split('=')
-#       folds.append((axis, int(val)))
+maxX, maxY = getMaxes(marks)
 
-marks = []
-folds = []
-for line in lines.split('\n'):
-  if not line.startswith('fold') and line != '':
-    x,y = line.strip().split(',')
-    marks.append((int(x),int(y)))
-  elif line.startswith('fold'):
-    axis, val = line.split()[2].split('=')
-    folds.append((axis, int(val)))
-
-max_x, max_y = findMaxes(marks)
-print(max_x, max_y)
-paper = np.zeros([max_y + 1, max_x + 1])
-
+paper = np.zeros((maxY + 1, maxX + 1))
 for coord in marks:
   paper[coord[1]][coord[0]] = 1
 
-# if folds[0][0] == 'y':
-#   marks = foldHorz(folds[0][1], marks)
-#   for coord in marks:
-#     paper[coord[1]][coord[0]] = 1
-#   paper = np.delete(paper, list(range(folds[0][1], max_y + 1)), 0)
-# if folds[0][0] == 'x':
-#   marks = foldVert(folds[0][1], marks)
-#   for coord in marks:
-#     paper[coord[1]][coord[0]] = 1
-#   paper = np.delete(paper, list(range(0, folds[0][1] + 1)), 1)
-
-# print(paper)
-
-for fold in folds:
-  print(fold)
+for i in range(0, len(folds)):
+  fold = folds[i]
   if fold[0] == 'y':
-    marks = foldHorz(fold[1], marks)
-    for coord in marks:
-      try:
-        paper[coord[1]][coord[0]] = 1
-      except:
-        continue
-    paper = np.delete(paper, list(range(fold[1], max_y + 1)), 0)
-  if fold[0] == 'x':
     marks = foldVert(fold[1], marks)
     for coord in marks:
-      try:
-        paper[coord[1]][coord[0]] = 1
-      except:
-        continue
-    paper = np.delete(paper, list(range(0, fold[1] + 1)), 1)
-  
-print(paper)
+      paper[coord[1]][coord[0]] = 1
+    paper = np.delete(paper, slice(fold[1], maxY + 1), 0)
+  if fold[0] == 'x':
+    marks = foldHorz(fold[1], marks)
+    for coord in marks:
+      paper[coord[1]][coord[0]] = 1
+    paper = np.delete(paper, slice(fold[1], maxX + 1), 1)
+  if i == 0:
+    print(f'The answer to Part 1 is {np.sum(paper)}')
+  maxY, maxX = paper.shape
+
+code_paper = np.full((maxY, maxX), '.')
+
+for coord in marks:
+  code_paper[coord[1]][coord[0]] = '#'
+
+for i in range(0, maxX, 5):
+  print(code_paper[0:7, i:i + 5])
+  print('')
