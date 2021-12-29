1e9fbba85aba666a4a5362dc707b57cc07b4f6a3
PNJaenichen <PNJaenichen@gmail.com>
2021-12-28 22:36:55 -0500

    completed day 14 with a little help

--- projects/adventOfCode/2021/Day14.py
+++ projects/adventOfCode/2021/Day14.py
@@ -32,29 +32,29 @@ def maxMinChar(counters):
       min = count
   return max, min
   
-input = '''NNCB
+# input = '''NNCB
 
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C'''
+# CH -> B
+# HH -> N
+# CB -> H
+# NH -> C
+# HB -> C
+# HC -> B
+# HN -> C
+# NN -> C
+# BH -> H
+# NC -> B
+# NB -> B
+# BN -> B
+# BB -> N
+# BC -> B
+# CC -> N
+# CN -> C'''
 
-# with open('day14.txt') as inp_lines:
-#   input = ''
-#   for i in inp_lines.readlines():
-#     input += i
+with open('day14.txt') as inp_lines:
+  input = ''
+  for i in inp_lines.readlines():
+    input += i
 
 lines = input.split('\n')
 
@@ -64,6 +64,7 @@ letters = []
 letter_counts = []
 
 for line in lines[2:]:
+  print(line)
   split_line = line.strip().split(' -> ')
   pair_dict[split_line[0]] = split_line[1]
   for char in [char for char in split_line[0]]:
@@ -76,5 +77,52 @@ for char in letters:
   letter_counts.append(final_inp.count(char))
 
 max, min = maxMinChar(list(zip(letters, letter_counts)))
-print(max[1] - min[1])
+print(f'The answer to part 1 is {max[1] - min[1]}')
 
+singles = set()
+pairs = {}
+rules = {}
+counts = {}
+
+start_code = lines[0]
+
+for line in lines[2:]:
+  pair, inp = line.strip().split(' -> ')
+  singles |= {pair[0], pair[1]}
+  pairs[pair] = 0
+  rules[pair] = inp
+  
+for count in singles:
+  counts[count] = 0
+  
+for char in start_code:
+  counts[char] += 1
+
+for pair in getInputPairs(start_code):
+  pairs[pair] += 1
+
+for _ in range(40):
+  new_dict = {pair:0 for pair in pairs.keys()}
+  for k,v in pairs.items():
+    if v > 0:
+      pair_one, pair_two = [char for char in k]
+      in_char = rules[k]
+      new_dict[pair_one + in_char] += v
+      new_dict[in_char + pair_two] += v
+      counts[in_char] += v
+      pairs[k] = 0
+  for k,v in new_dict.items():
+    pairs[k] += v
+
+def highLow(cnt_dict):
+  max = cnt_dict[list(cnt_dict.keys())[0]]
+  min = cnt_dict[list(cnt_dict.keys())[0]]
+  for k,v in cnt_dict.items():
+    if v > max:
+      max = v
+    elif v < min:
+      min = v
+  return max, min
+
+max, min = highLow(counts)
+print(f'The answer to part 2 is {max - min}')
