a80584c5fee7459fce2b9aec2d394609b964deae
PNJaenichen <PNJaenichen@gmail.com>
2021-12-29 19:54:46 -0500

    set up tabs for OWS tools

--- projects/OWSadjudicator/adjudicator.js
+++ projects/OWSadjudicator/adjudicator.js
@@ -1,3 +1,29 @@
+// Tabbing, code from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openTool(evt, toolName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+  
+  // Get all elements with class 'tabcontent' and hide them
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+
+  // Get all elements with class 'tabcontent' and remove the class 'active'
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+
+  // Show the current tab, and add an 'active' class to the bottom that opened the tab
+  document.getElementById(toolName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 // Dice functions for all tools
 
 function demoteAll(missiles) {
