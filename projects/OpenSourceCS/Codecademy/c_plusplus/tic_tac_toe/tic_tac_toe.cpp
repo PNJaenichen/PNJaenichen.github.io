46ade0c37f96d9b9dcaa62caf67b39c27ae41167
PNJaenichen <PNJaenichen@gmail.com>
2022-03-20 22:18:22 -0400

    started C++ tic tac toe project

--- projects/OpenSourceCS/Codecademy/c_plusplus/tic_tac_toe/tic_tac_toe.cpp
+++ projects/OpenSourceCS/Codecademy/c_plusplus/tic_tac_toe/tic_tac_toe.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 /*
 
@@ -15,3 +16,13 @@
 - use functions to be modular
 
 */
+
+int main() {
+  std::vector<std::string> player_picks = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+  std::cout << " " << player_picks[0] << " | " << player_picks[1] << " | " << player_picks[2] << " \n"; 
+  std::cout << "-----------\n"; 
+  std::cout << " " << player_picks[3] << " | " << player_picks[4] << " | " << player_picks[5] << " \n";
+  std::cout << "-----------\n";
+  std::cout << " " << player_picks[6] << " | " << player_picks[7] << " | " << player_picks[8] << " \n";
+  return 0;
+}
