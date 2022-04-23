b2a823c2088ce27c91f44bda74b127f3073d5850
PNJaenichen <PNJaenichen@gmail.com>
2022-04-22 23:39:40 -0400

    working on router project from Codecademy

---
+++ projects/OpenSourceCS/Codecademy/react-router-lesson-starter/src/components/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Route, Link } from "react-router-dom";
+import { selectCurrentUser, selectIsLoggedIn } from "../features/session//sessionSlice";
+import EditProfileForm from "./EditProfileForm";
+
+export default function Profile () {
+  const currentUser = useSelector(selectCurrentUser)
+  const loggedIn = useSelector(selectIsLoggedIn);
+
+  // call useRouteMatch() to get the url and path
+  
+  // use loggedIn to return a Redirect
+  
+  return (
+    <main>
+      <h1>{currentUser.username}</h1>
+      <Link to={"/profile/edit"}>Edit</Link>
+      <Route path='profile/edit'>
+        <EditProfileForm />
+      </Route>
+    </main>
+  )
+}
