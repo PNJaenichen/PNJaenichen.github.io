c33520e6570ff50206facb4281dd0c66fd353b85
PNJaen <PNJaenichen@gmail.com>
2022-04-28 22:09:13 -0700

    started further development of new OWS tool

--- projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
+++ projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
@@ -1,6 +1,46 @@
 const Adjudicate = {
   dieRoller(sides) {
-    return Math.floor(Math.random() * sides) + 1;
+    if ([4, 6, 8, 10, 12, 16, 20].includes(sides)) {
+      return Math.floor(Math.random() * sides) + 1;
+    } else {
+      throw new TypeError('No die with that number of sides');
+    }
+  },
+  
+  promoteAll(dice) {
+    if (dice['16']) {
+      if(dice['20']) {
+        dice['20'] += dice['16'];
+      } else {
+        dice['20'] = dice['16'];
+      }
+      delete dice['16'];
+    } 
+    if (dice['12']) {
+      dice['16'] = dice['12'];
+      delete dice['12'];
+    }
+    if (dice['10']) {
+      dice['12'] = dice['10'];
+      delete dice['12'];
+    }
+    if (dice['8']) {
+      dice['10'] = dice['8'];
+      delete dice['8'];
+    }
+    if (dice['6']) {
+      dice['8'] = dice['6'];
+      delete dice['6'];
+    }
+    if (dice['4']) {
+      dice['6'] = dice['4'];
+      delete dice['4'];
+    }
+    return dice;
+  },
+
+  promoteOne(dice) {
+    return {'6': 1};
   }
 };
 
--- projects/OpenSourceCS/Codecademy/node.js/test/test.js
+++ projects/OpenSourceCS/Codecademy/node.js/test/test.js
@@ -3,6 +3,7 @@ var Adjudicate = require('..\\OWS_Adjudicator_v18');
 
 describe('Dice Roller', () => {
   const minValue = 1;
+  const dieSides = [4, 6, 8, 10, 12, 16, 20];
   const testArray = (dice) => {
     const resultArray = [];
     for (let i = 0; i < 5; i++) {
@@ -10,46 +11,61 @@ describe('Dice Roller', () => {
     }
     return resultArray;
   };
-  describe('on a d4', () => {
-    const maxValue = 4;
-    const results1 = testArray(maxValue);
-    const results2 = testArray(maxValue);
-    it('returns random results', () => {
-      assert.notDeepStrictEqual(results1, results2);
+  describe('only allows specific sided dice', () => {
+    it('returns error if outside paramaters', () => {
+      assert.throws(() => testArray(0), TypeError)
     });
-    it('returns values between 1 and 4', () => {
-      const results1_check = results1.filter(x => x < 1 || x > maxValue);
-      const results2_check = results2.filter(x => x < 1 || x > maxValue);
-      assert.ok((results1_check.length === 0) && (results2_check.length === 0), 'Values were out of range.');
+  });
+  for (let i = 0; i < dieSides.length; i++) {
+    describe(`on a d${dieSides[i]}`, () => {
+      const maxValue = dieSides[i];
+      const results1 = testArray(maxValue);
+      const results2 = testArray(maxValue);
+      it('returns random results', () => {
+        assert.notDeepStrictEqual(results1, results2);
+      });
+      it(`returns values between 1 and ${dieSides[i]}`, () => {
+        const results1_check = results1.filter(x => x < 1 || x > maxValue);
+        const results2_check = results2.filter(x => x < 1 || x > maxValue);
+        assert.ok((results1_check.length === 0) && (results2_check.length === 0), 'Values were out of range.');
+      })
+    });
+  }; 
+});
+
+describe('Dice Promotion', () => {
+  describe('of all dice', () => {
+    it('that are the same and not a d20', () => {
+      const startPool = {'4': 3};
+      const promoPool = Adjudicate.promoteAll(startPool);
+      const expectPool = {'6': 3};
+      assert.deepStrictEqual(promoPool, expectPool);
+    });
+    it('and does not promote d20s', () => {
+      const startPool = {'20': 3};
+      const promoPool = Adjudicate.promoteAll(startPool);
+      const expectPool = {'20': 3}
+      assert.deepStrictEqual(promoPool, expectPool);
+    });
+    it('that are different and not a d20', () => {
+      const startPool = {'4': 2, '8': 3, '16': 1};
+      const promoPool = Adjudicate.promoteAll(startPool);
+      const expectPool = {'6': 2, '10': 3, '20': 1};
+      assert.deepStrictEqual(promoPool, expectPool);
+    });
+    it('that are different and includes a d20', () => {
+      const startPool = {'8': 2, '12': 3, '20': 1};
+      const promoPool = Adjudicate.promoteAll(startPool);
+      const expectPool = {'10': 2, '16': 3, '20': 1};
+      assert.deepStrictEqual(promoPool, expectPool);
     })
   });
-})
-
-/* describe('Calculate', () => {
-  describe('.factorial', () => {
-    it('returns 120 with input of 5', () => {
-      const expectedResult = 120;
-      const inputValue = 5;
-
-      const result = Calculate.factorial(inputValue);
-
-      assert.strictEqual(result, expectedResult);
-    });
-    it('returns 6 with input of 3', () => {
-      const expectedResult = 6;
-      const inputValue = 3;
-
-      const result = Calculate.factorial(inputValue);
-      
-      assert.strictEqual(result, expectedResult);
-    });
-    it('results 1 with input of 0', () => {
-      const expectedResult = 1;
-      const inputValue = 0;
-
-      const result = Calculate.factorial(inputValue);
-
-      assert.strictEqual(result, expectedResult);
-    });
+  describe('of one die', () => {
+    it('and promotes properly', () => {
+      const startPool = {'4': 1};
+      const promoPool = Adjudicate.promoteOne(startPool);
+      const expectPool = {'6': 1};
+      assert.deepStrictEqual(promoPool, expectPool);
+    })
   });
-}); */
+});
