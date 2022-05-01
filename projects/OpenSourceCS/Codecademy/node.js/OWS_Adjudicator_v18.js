2edd0fad4682890b229be7d154d9f01c3bf388d8
PNJaen <PNJaenichen@gmail.com>
2022-05-01 05:33:40 -0700

    flushing out rrequirements for different tools

--- projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
+++ projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
@@ -148,6 +148,132 @@ const Adjudicate = {
     }
     return new_dice;
   }
+
+  /*
+  Missile Strikes
+
+  Hypersonic - no CAP no defensive missiles
+  Ballistic - no CAP, if beat grey shield roll against red shield
+
+  Hyper and Ballistic roll d4 to determine hits
+
+  SIGINT/EMSO promotes attacks
+
+  Active/Passive CMs demotes attacks
+
+  Long Rnage attacks with Disrupted PNT demotes attacks
+
+  SOF TA promotes against a single target, this can be used with SOF SR on the
+  same target
+
+  Need target unit steps, base defense, and defensive missiles
+  Need inbound offensive capability
+
+  Critical Hit ... if value rolled is double the defense value then 2 hits are done
+  if max value rolled on d12 and above, then 3 hits are done
+
+  unengaged CAP demote at 1:1 ratio
+  excess defensive missiles demote at 1:1 ratio
+
+  US E-2D CEC capability is double demotion
+
+  Against Fuel Points or ASPs each hit does D6 damage
+
+  */
+
+  /*
+  ISR Detections
+
+  Number of units being detected, may want to break them out for OIE tokens
+
+  SOF SR promotes against a single target
+
+  Active/Passive CMs demote detections
+  SIGINT/EMSO promotes detections
+
+  Combine assets
+  */
+
+  /*
+  ASW 
+
+  Defense and Detection together
+  Littoral Waters, Running Silent, Caviation, Multiple Assets
+  Promotions/Demotions
+  */
+
+  /*
+  Torpedo Attack
+
+  Similar to ASW but add Target Steps, and Target Defense, Torp Strength
+  and remove Running Silent
+
+  */
+
+  // 
+
+  /*
+  Ground Combat
+
+  Tactical map: Crowded hex (8+ steps) promote attacks into hex
+                Over Crowded (10+ steps) double promote attacks into hex
+
+  Fires Effects Table
+
+  Target     Open           Forest        Mountain        Urban
+            1 hit Supp    2 hits supp   1 hit supp      2 hits supp
+  Armor     2 hits dmg    3 hits dmg    2 hits dmg      3 hits dmg
+            3 hits kill   4 hits kill   Air PGM Promo   Air PGM Promo
+
+            1 hit supp    2 hits supp   2 hits supp     2 hits supp
+  Inf       2 hits dmg    3 hits dmg    3 hits dmg      3 hits dmg
+            3 hits kill   4 hits kill   4 hits kill     5 hits kill
+                                                        Hardened Tgt
+
+            1 hit dmg     1 hit supp    1 hit supp      1 hit supp
+  arty/     & suppress    2 hits dmg    2 hits dmg      2 hits dmg
+    miss                  3 hits kill   3 hits kill     3 hits kill
+                                        Air PGM Promo   Air PGM Promo
+  
+       Actioning Unit                  Target Unit Is
+  Defender              +2        IDF Supp              -1/each
+  3 Axis Att            +2        CAS Supp              -1/each
+  2 Axis Att            +1        Defense               -1
+  ME                    +1        Fortified             -2
+  SIGINT/EMSO           +1        Act/Pass CM           -1
+  IDF Supp              +1/each   Suppressed            +1
+  CAS                   +1/each   Crowded Hex           +1
+  Tnk v lInf in Open    +1        Over Crowded Hex      +2
+  lInf v Tank not Open  +1        RSO&I                 +1
+  SOF Enabled           +1        River/Abn/Amphib Aslt +2
+  Troop Quality         +/-1
+  Disrupted C2/PNT      -1/each
+  Suppressed            -1
+  In RSO&I              -1
+  Extended Supply       -1
+  Wx=Lt Rain/Snow       -1
+  Wx=Storm              -2
+  Isolated              -3
+  River/Abn/Amphib Aslt -2
+  Crowded Atk           -1
+  Over Crowded Atk      -2
+
+
+  Lvl of Supp    Open    Forest/Mtn    Urban     Dense Urban
+     >= +7       3 P        3 P         3 P           2 P
+       +6        3 P        2 P         2 P           2 P
+       +5        2 P        2 P         2 P           1 P
+       +4        2 P        2 P         1 P            -
+       +3        1 P        1 P         1 P            -
+       +2        1 P         -           -            1 D
+       +1         -          -          1 D           1 D
+       0         1 D        1 D         1 D           2 D
+       -1        1 D        1 D         2 D           3 D
+       -2        2 D        2 D         3 D           3 D
+       -3        3 D        3 D         3 D           3 D<
+     <= -4       3 D        3 D         4 D           4 D
+     
+  */        
 };
 
 module.exports = Adjudicate;
