70aa06309fd2d8280eb391e71d769699dfdecc6d
PNJaenichen <PNJaenichen@gmail.com>
2022-05-05 23:25:03 -0400

    Starting over with the adjudicator

--- projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
+++ projects/OpenSourceCS/Codecademy/node.js/OWS_Adjudicator_v18.js
@@ -1,177 +1,103 @@
+
 function dieRoller(sides) {
-  if ([4, 6, 8, 10, 12, 16, 20].includes(sides)) {
-    return Math.floor(Math.random() * sides) + 1;
-  } else {
-    throw new TypeError('No die with that number of sides');
-  }
-};
-
-const pool_adjuster = {
-  promoteAll(dice) {
-    if (dice['16']) {
-      if(dice['20']) {
-        dice['20'] += dice['16'];
-      } else {
-        dice['20'] = dice['16'];
-      }
-      delete dice['16'];
-    } 
-    if (dice['12']) {
-      dice['16'] = dice['12'];
-      delete dice['12'];
-    }
-    if (dice['10']) {
-      dice['12'] = dice['10'];
-      delete dice['12'];
-    }
-    if (dice['8']) {
-      dice['10'] = dice['8'];
-      delete dice['8'];
-    }
-    if (dice['6']) {
-      dice['8'] = dice['6'];
-      delete dice['6'];
-    }
-    if (dice['4']) {
-      dice['6'] = dice['4'];
-      delete dice['4'];
-    }
-    return dice;
-  },
-
-  promoteOne(dice) {
-    if (dice['4']) {
-      dice['6'] ? dice['6'] += 1 : dice['6'] = 1;
-      dice['4'] > 1 ? dice['4'] -= 1 : delete dice['4'];
-    } else if (dice['6']) {
-      dice['8'] ? dice['8'] += 1 : dice['8'] = 1;
-      dice['6'] > 1 ? dice['6'] -= 1 : delete dice['6'];
-    } else if (dice['8']) {
-      dice['10'] ? dice['10'] += 1 : dice['10'] = 1;
-      dice['8'] > 1 ? dice['8'] -= 1 : delete dice['8'];
-    } else if (dice['10']) {
-      dice['12'] ? dice['12'] += 1 : dice['12'] = 1;
-      dice['10'] > 1 ? dice['10'] -= 1 : delete dice['10'];
-    } else if (dice['12']) {
-      dice['16'] ? dice['16'] += 1 : dice['16'] = 1;
-      dice['12'] > 1 ? dice['12'] -= 1 : delete dice['12'];
-    } else if (dice['16']) {
-      dice['20'] ? dice['20'] += 1 : dice['20'] = 1;
-      dice['16'] > 1 ? dice['16'] -= 1 : delete dice['16'];
-    }
-    return dice;
-  },
-
-  demoteAll(dice) {
-    if (dice['4']) {
-      delete dice['4'];
-    }
-    if (dice['6']) {
-      dice['4'] = dice['6'];
-      delete dice['6'];
-    }
-    if (dice['8']) {
-      dice['6'] = dice['8'];
-      delete dice['8'];
-    }
-    if (dice['10']) {
-      dice['8'] = dice['10'];
-      delete dice['10'];
-    }
-    if (dice['12']) {
-      dice['10'] = dice['12'];
-      delete dice['12'];
-    }
-    if (dice['16']) {
-      dice['12'] = dice['16'];
-      delete dice['16'];
-    }
-    if (dice['20']) {
-      dice['16'] = dice['20'];
-      delete dice['20'];
-    }
-    return dice;
-  },
-
-  demoteOne(dice) {
-    if (dice['20']) {
-      dice['16'] ? dice['16'] += 1 : dice['16'] = 1;
-      dice['20'] > 1 ? dice['20'] -= 1 : delete dice['20'];
-    } else if (dice['16']) {
-      dice['12'] ? dice['12'] += 1 : dice['12'] = 1;
-      dice['16'] > 1 ? dice['16'] -= 1 : delete dice['16'];
-    } else if (dice['12']) {
-      dice['10'] ? dice['10'] += 1 : dice['10'] = 1;
-      dice['12'] > 1 ? dice['12'] -= 1 : delete dice['12'];
-    } else if (dice['10']) {
-      dice['8'] ? dice['8'] += 1 : dice['8'] = 1;
-      dice['10'] > 1 ? dice['10'] -= 1 : delete dice['10'];
-    } else if (dice['8']) {
-      dice['6'] ? dice['6'] += 1 : dice['6'] = 1;
-      dice['8'] > 1 ? dice['8'] -= 1 : delete dice['8'];
-    } else if (dice['6']) {
-      dice['4'] ? dice['4'] += 1 : dice['4'] = 1;
-      dice['6'] > 1 ? dice['6'] -= 1 : delete dice['6'];
-    } else if (dice['4']) {
-      dice['4'] > 1 ? dice['4'] -= 1 : delete dice['4'];
-    }
-    return dice;
-  },
-
-  allForOne(dice, ASW = false) {
-    const total_dice = Object.values(dice).reduce((prev, curr) => prev + curr, 0);
-    const die_sides = Object.keys(dice).map(die => parseInt(die));
-    const high_side = Math.max(...die_sides);
-    let new_dice = {};
-    if (total_dice > 1) {
-      if (high_side === 20 && total_dice > 2) {
-        new_dice['20'] = dice['20'] + 1;
-      } else {
-        switch (high_side) {
-          case 16:
-            if (ASW) {
-              new_dice['16'] = dice['16'] + 1;
-            } else {
-              new_dice['20'] = 1;
-            }
-            break;
-          case 12:
-            new_dice['16'] = 1;
-            break;
-          default:
-            new_dice[(high_side + 2).toString()] = 1;
-            break;
-        }
-      }
-    } else {
-      new_dice = dice;
-    }
-    return new_dice;
-  }
-       
-};
-
-const missile_attack = {
-  pool_adjust(incoming, singles, promotions, demotions) {
-    let missiles = incoming;
-    const all_diff = promotions - demotions;
-    if (all_diff > 0) {
-      for (let i = 0; i < all_diff; i++) {
-        pool_adjuster.promoteAll(missiles);
-      }
-    } else if (all_diff < 0) {
-      for (let i = 0; i > all_diff; i--) {
-        pool_adjuster.demoteAll(missiles);
-      }
-    } 
-    for (let i = 0; i < singles; i++) {
-      pool_adjuster.demoteOne(missiles);
-    }
-    return missiles
-   }
+  return Math.ceil(Math.random() * sides)
 }
 
-module.exports = { dieRoller, pool_adjuster, missile_attack };
+function promoteAll(dice) {
+  if (dice['16']) {
+    dice['20'] ? dice['20'] += dice['16'] : dice['20'] = dice['16'];
+    delete dice['16']
+  }
+  if (dice['12']) {
+    dice['16'] ? dice['16'] += dice['12'] : dice['16'] = dice['12'];
+    delete dice['12']
+  }
+  if (dice['10']) {
+    dice['12'] ? dice['12'] += dice['10'] : dice['12'] = dice['10'];
+    delete dice['10']
+  }
+  if (dice['8']) {
+    dice['10'] ? dice['10'] += dice['8'] : dice['10'] = dice['8'];
+    delete dice['8']
+  }
+  if (dice['6']) {
+    dice['8'] ? dice['8'] += dice['6'] : dice['8'] = dice['6'];
+    delete dice['6']
+  }
+  if (dice['4']) {
+    dice['20'] ? dice['20'] += dice['16'] : dice['20'] = dice['16'];
+    delete dice['16']
+  }
+  return dice
+}
+
+function promoteOne(dice) {
+  let temp_20 = 0;
+  if (dice['20']) {
+    temp_20 = dice['20']
+    delete dice['20']
+  } 
+  const high_die = Math.max(...Object.keys(dice).map(x => parseInt(x)));
+  if (high_die === 16) {
+    dice['20'] = temp_20 + 1;
+    dice['16'] === 1 ? delete dice['16'] : dice['16'] -= 1;
+  } else {
+    let step = 0;
+    high_die === 12 ? step = 4 : step = 2;
+    dice[(high_die + step).toString()] = 1;
+    dice[high_die.toString()] === 1 ? delete dice[high_die.toString()] : dice[high_die.toString()] -= 1;
+  }
+  return dice
+}
+
+function demoteAll(dice) {
+  if (dice['4']) {
+    delete dice['4']
+  }
+  if (dice['6']) {
+    dice['4'] = dice['6'];
+    delete dice['6']
+  }
+  if (dice['8']) {
+    dice['6'] = dice['8'];
+    delete dice['8']
+  }
+  if (dice['10']) {
+    dice['8'] = dice['10'];
+    delete dice['10']
+  }
+  if (dice['12']) {
+    dice['10'] = dice['12'];
+    delete dice['12']
+  }
+  if (dice['16']) {
+    dice['12'] = dice['16'];
+    delete dice['16']
+  }
+  if (dice['20']) {
+    dice['16'] = dice['20'];
+    delete dice['20']
+  }
+  return dice
+}
+
+function demoteOne(dice) {
+  const high_die = Math.max(...Object.keys(dice).map(x => parseInt(x)));
+  if (high_die === 4) {
+    dice['4'] === 1 ? delete dice['4'] : dice['4'] -= 1;
+  } else {
+    let step = 0;
+    high_die === 16 || high_die === 20 ? step = 4 : step = 2;
+    dice[(high_die - step).toString()] ? dice[(high_die - step).toString()] += 1 : dice[(high_die - step).toString()] = 1;
+    dice[(high_die).toString()] === 1 ? delete dice[(high_die).toString()] : dice[(high_die).toString()] -= 1;
+  }
+  return dice
+}
+
+function allForOne(dice) {
+  return dice
+}
+module.exports = { dieRoller, promoteAll, promoteOne, demoteAll, demoteOne, allForOne };
 
 
   /*
--- projects/OpenSourceCS/Codecademy/node.js/test/test.js
+++ projects/OpenSourceCS/Codecademy/node.js/test/test.js
@@ -1,176 +1,127 @@
 var assert = require("assert");
-var { dieRoller, pool_adjuster, missile_attack } = require('..\\OWS_Adjudicator_v18');
+var { dieRoller, promoteAll, demoteAll, promoteOne, demoteOne, allForOne } = require('..\\OWS_Adjudicator_v18');
 
-describe('Dice Roller', () => {
-  const dieSides = [4, 6, 8, 10, 12, 16, 20];
-  const testArray = (dice) => {
-    const resultArray = [];
-    for (let i = 0; i < 5; i++) {
-      resultArray.push(dieRoller(dice));
-    }
-    return resultArray;
-  };
-  describe('only allows specific sided dice', () => {
-    it('returns error if outside paramaters', () => {
-      assert.throws(() => testArray(0), TypeError)
-    });
-  });
-  for (let i = 0; i < dieSides.length; i++) {
-    describe(`on a d${dieSides[i]}`, () => {
-      const maxValue = dieSides[i];
-      const results1 = testArray(maxValue);
-      const results2 = testArray(maxValue);
-      it('returns random results', () => {
-        assert.notDeepStrictEqual(results1, results2);
-      });
-      it(`returns values between 1 and ${dieSides[i]}`, () => {
-        const results1_check = results1.filter(x => x < 1 || x > maxValue);
-        const results2_check = results2.filter(x => x < 1 || x > maxValue);
-        assert.ok((results1_check.length === 0) && (results2_check.length === 0), 'Values were out of range.');
+describe('Dice Roller creates random numbers', () => {
+  describe('that stay within 1', () => {
+    const tests = [4, 6, 8, 10, 12, 16, 20];
+    tests.forEach(sides => {
+      const dieResults = [];
+      for (let i = 0; i < 10; i++) {
+        dieResults.push(dieRoller(sides));
+      }
+      it(`and a ${sides} on a d${sides}`, () => {
+        assert.ok(dieResults.filter(x => x > sides).length === 0);
       })
-    });
-  }; 
-});
-
-describe('Dice Promotion', () => {
-  describe('of all dice', () => {
-    it('that are the same and not a d20', () => {
-      const startPool = {'4': 3};
-      const promoPool = pool_adjuster.promoteAll(startPool);
-      const expectPool = {'6': 3};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('and does not promote d20s', () => {
-      const startPool = {'20': 3};
-      const promoPool = pool_adjuster.promoteAll(startPool);
-      const expectPool = {'20': 3}
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('that are different and not a d20', () => {
-      const startPool = {'4': 2, '8': 3, '16': 1};
-      const promoPool = pool_adjuster.promoteAll(startPool);
-      const expectPool = {'6': 2, '10': 3, '20': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('that are different and includes a d20', () => {
-      const startPool = {'8': 2, '12': 3, '20': 1};
-      const promoPool = pool_adjuster.promoteAll(startPool);
-      const expectPool = {'10': 2, '16': 3, '20': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    })
-  });
-  describe('of one die', () => {
-    it('and promotes a single die pool properly', () => {
-      const startPool = {'4': 1};
-      const promoPool = pool_adjuster.promoteOne(startPool);
-      const expectPool = {'6': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('and promotes a multiple dice pool properly', () => {
-      const startPool = {'10': 2, '12': 1};
-      const promoPool = pool_adjuster.promoteOne(startPool);
-      const expectPool = {'10': 1, '12': 2};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('and it does not promote a d20', () => {
-      const startPool = {'20': 1};
-      const promoPool = pool_adjuster.promoteOne(startPool);
-      const expectPool = {'20': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    })
-  });
-  describe('combines all dice', () => {
-    it('into one of the next highest die', () => {
-      const startPool = {'8': 1, '10': 2};
-      const promoPool = pool_adjuster.allForOne(startPool);
-      const expectPool = {'12': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('into a single die but ignores d20s', () => {
-      const startPool = {'8': 1, '10': 2, '20': 1};
-      const promoPool = pool_adjuster.allForOne(startPool);
-      const expectPool = {'20': 2};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('but only if more than one die is in the pool', () => {
-      const startPool = {'6': 1};
-      const promoPool = pool_adjuster.allForOne(startPool);
-      const expectPool = {'6': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('but never returns more than a d16 for ASW', () => {
-      const startPool = {'8': 1, '10': 2, '16': 1};
-      const promoPool = pool_adjuster.allForOne(startPool, true);
-      const expectPool = {'16': 2}
-      assert.deepStrictEqual(promoPool, expectPool);
     })
   })
 });
 
-describe('Dice Demotion', () => {
-  describe('of all dice', () => {
-    it('that are the same and not a d4', () => {
-      const startPool = {'8': 3};
-      const promoPool = pool_adjuster.demoteAll(startPool);
-      const expectPool = {'6': 3};
-      assert.deepStrictEqual(promoPool, expectPool);
+describe('Dice Pool Adjustments', () => {
+  describe('promotes all dice appropriately', () => {
+    it('when the dice are all the same value', () => {
+      const startPool = {'6': 2};
+      const adjPool = promoteAll(startPool)
+      const expectedPool = {'8': 2};
+      assert.deepStrictEqual(adjPool, expectedPool);
     });
-    it('and removes d4s', () => {
-      const startPool = {'4': 3};
-      const promoPool = pool_adjuster.demoteAll(startPool);
-      const expectPool = {};
-      assert.deepStrictEqual(promoPool, expectPool)
+    it('when the dice are all different values', () => {
+      const startPool = {'6': 2, '8': 3, '10': 1};
+      const adjPool = promoteAll(startPool)
+      const expectedPool = {'8': 2, '10': 3, '12': 1};
+      assert.deepStrictEqual(adjPool, expectedPool)
     });
-    it('that are different and not a d4', () => {
-      const startPool = {'8': 3, '10': 1, '20': 2};
-      const promoPool = pool_adjuster.demoteAll(startPool);
-      const expectPool = {'6': 3, '8': 1, '16': 2};
-      assert.deepStrictEqual(promoPool, expectPool);
-    });
-    it('that are dfifferent and removes d4s', () => {
-      const startPool = {'4': 3, '10': 1, '20': 2};
-      const promoPool = pool_adjuster.demoteAll(startPool);
-      const expectPool = {'8': 1, '16': 2};
-      assert.deepStrictEqual(promoPool, expectPool);
+    it('when a d20 in the pool', () => {
+      const startPool = {'6': 2, '10': 3, '16': 1, '20': 1};
+      const adjPool = promoteAll(startPool)
+      const expectedPool = {'8': 2, '12': 3, '20': 2};
+      assert.deepStrictEqual(adjPool, expectedPool)
     });
   });
-  describe('of one die', () => {
-    it('and demotes a single die pool properly', () => {
-      const startPool = {'8': 1};
-      const promoPool = pool_adjuster.demoteOne(startPool);
-      const expectPool = {'6': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
+  describe('promotes a single die appropriately', () => {
+    it('when the dice are all the same value', () => {
+      const startPool = {'6': 2};
+      const adjPool = promoteOne(startPool);
+      const expectedPool = {'6': 1, '8': 1};
+      assert.deepStrictEqual(adjPool, expectedPool);
     });
-    it('and demotes a multiple dice pool properly', () => {
-      const startPool = {'10': 2, '12': 1};
-      const promoPool = pool_adjuster.demoteOne(startPool);
-      const expectPool = {'10': 3};
-      assert.deepStrictEqual(promoPool, expectPool);
+    it('when the dice are all different values', () => {
+      const startPool = {'6': 2, '8': 3, '10': 1};
+      const adjPool = promoteOne(startPool)
+      const expectedPool = {'6': 2, '8': 3, '12': 1};
+      assert.deepStrictEqual(adjPool, expectedPool)
     });
-    it('and it removes a d4 if it is the only one', () => {
-      const startPool = {'4': 1};
-      const promoPool = pool_adjuster.demoteOne(startPool);
-      const expectPool = {};
-      assert.deepStrictEqual(promoPool, expectPool);
+    it('when a d20 in the pool', () => {
+      const startPool = {'6': 2, '10': 3, '16': 1, '20': 1};
+      const adjPool = promoteOne(startPool)
+      const expectedPool = {'6': 2, '10': 3, '20': 2};
+      assert.deepStrictEqual(adjPool, expectedPool)
     });
-    it('and it removes only a single d4 if more than one', () => {
-      const startPool = {'4': 2};
-      const promoPool = pool_adjuster.demoteOne(startPool);
-      const expectPool = {'4': 1};
-      assert.deepStrictEqual(promoPool, expectPool);
+  });
+  describe('demotes all dice appropriately', () => {
+    it('when the dice are all the same value', () => {
+      const startPool = {'6': 2};
+      const adjPool = demoteAll(startPool)
+      const expectedPool = {'4': 2};
+      assert.deepStrictEqual(adjPool, expectedPool);
+    });
+    it('when the dice are all different values', () => {
+      const startPool = {'6': 2, '8': 3, '10': 1};
+      const adjPool = demoteAll(startPool)
+      const expectedPool = {'4': 2, '6': 3, '8': 1};
+      assert.deepStrictEqual(adjPool, expectedPool)
+    });
+    it('removes any d4s from the pool', () => {
+      const startPool = {'4': 2, '8': 3, '12': 1};
+      const adjPool = demoteAll(startPool)
+      const expectedPool = {'6': 3, '10': 1};
+      assert.deepStrictEqual(adjPool, expectedPool)
     })
   });
-});
-
-// incoming, singles, promotions, demotions, hyper, ballistic
-
-describe('Missile Strike', () => {
-  describe('makes adjustments to dice pool', () => {
-    it('returns correctly if no adjustments', () => {
-      assert.deepStrictEqual(missile_attack.pool_adjust({'10': 2}, 0, 0, 0), {'10': 2});
+  describe('demotes a single die appropriately', () => {
+    it('when the dice are all the same value', () => {
+      const startPool = {'6': 2};
+      const adjPool = demoteOne(startPool);
+      const expectedPool = {'4': 1, '6': 1};
+      assert.deepStrictEqual(adjPool, expectedPool);
     });
-    it('returns correctly if only promotions made', () => {
-      assert.deepStrictEqual(missile_attack.pool_adjust({'10': 2}, 0, 2, 0), {'16': 2})
-    }) 
+    it('when the dice are all different values', () => {
+      const startPool = {'6': 2, '8': 3, '10': 1};
+      const adjPool = demoteOne(startPool)
+      const expectedPool = {'6': 2, '8': 4};
+      assert.deepStrictEqual(adjPool, expectedPool)
+    });
+    it('removes a d4 from the pool', () => {
+      const startPool = {'4': 2};
+      const adjPool = demoteOne(startPool)
+      const expectedPool = {'4': 1};
+      assert.deepStrictEqual(adjPool, expectedPool)
+    });
   });
-});
+  describe('promotes multiple dice into a single higher die', () => {
+    it('when the dice are all the same value', () => {
+      const startPool = {'6': 2};
+      const adjPool = allForOne(startPool);
+      const expectedPool = {'8': 1};
+      assert.deepStrictEqual(adjPool, expectedPool);
+    });
+    it('when the dice are all different valuees', () => {
+      const startPool = {'6': 2, '8': 3, '10': 1};
+      const adjPool = allForOne(startPool);
+      const expectedPool  = {'12': 1};
+      assert.deepStrictEqual(adjPool, expectedPool);
+    });
+    it('when a d20 present in the dice pool', () => {
+      const startPool = {'6': 2, '10': 3, '16': 1, '20': 1};
+      const adjPool = allForOne(startPool)
+      const expectedPool = {'20': 2};
+      assert.deepStrictEqual(adjPool, expectedPool)
+    });
+    it('when only one other die in the pool with a d20', () => {
+      const startPool = {'16': 1, '20': 1};
+      const adjPool = allForOne(startPool);
+      const expectedPool = {'16': 1, '20': 1};
+      assert.deepStrictEqual(adjPool, expectedPool);
+    })
+  });
+
+})
