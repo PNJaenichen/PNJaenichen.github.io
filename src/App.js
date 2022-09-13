018622389d4aef4c25be20f083501651ed146804
PNJaenichen <PNJaenichen@gmail.com>
2022-09-12 21:48:51 -0400

    updates to first Turn database

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import { Route, Switch } from "react-router-dom";
-import Blog from "./Containers/Blog/Blog";
-import Home from "./Containers/Home/Home"
-import Projects from "./Containers/Projects/Projects";
-import ResumeCV from "./Containers/ResumeCV/ResumeCV";
-import React from "react";
+  import { Route, Switch } from "react-router-dom";
+  import Blog from "./Containers/Blog/Blog";
+  import Home from "./Containers/Home/Home"
+  import Projects from "./Containers/Projects/Projects";
+  import ResumeCV from "./Containers/ResumeCV/ResumeCV";
+  import React from "react";
 
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/Blog" component={Blog} />
-          <Route exact path="/Projects" component={Projects} />
-          <Route path="/ResumeCV" component={ResumeCV} />
-        </Switch>
-      </div>
-    );
+  class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Blog" component={Blog} />
+            <Route exact path="/Projects" component={Projects} />
+            <Route path="/ResumeCV" component={ResumeCV} />
+          </Switch>
+        </div>
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
