3af3b72b576ebad81393d1b3ffc22b6f43942904
PNJaenichen <PNJaenichen@gmail.com>
2022-12-01 21:19:18 -0500

    day1 Advent of Code 2022

---
+++ projects/adventOfCode/2022/day1.py
@@ -0,0 +1,36 @@
+import functools
+
+testInput = '''1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+'''
+
+def carryCals(foods):
+  return functools.reduce(lambda x, y: x + y, list(map(lambda x: int(x), foods)))
+
+def splitElves(allCals):
+  replaceLines = allCals.replace("\n\n", ";").split(";")
+  return list(map(lambda x: x.splitlines(), replaceLines))
+
+with open('PNJaenichen.github.io\\projects\\adventOfCode\\2022\\day1input.txt') as bigList:
+  elves = splitElves(bigList.read())
+
+# elves = splitElves(testInput)
+
+calorieList = [carryCals(elf) for elf in elves]
+maxElf = [index for index, item in enumerate(calorieList) if item == max(calorieList)]
+print(maxElf, max(calorieList))
+
+calorieList.sort(reverse=True)
+print(functools.reduce(lambda x, y: x + y, calorieList[:3]))
